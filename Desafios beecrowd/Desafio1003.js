7d8ff0ce9de979c5a04ca108c38f8ddb9d4265d8
Victória <victoria.vasconcelos@hotmail.com>
2022-05-24 17:45:44 -0300

    Desafios beecrowd

--- Desafios beecrowd/Desafio1003.js
+++ Desafios beecrowd/Desafio1003.js
@@ -1,6 +1,6 @@
 var input = require("fs").readFileSync("stdin", "utf8");
 
-var valores = input.split("/n");
+var valores = input.split("\n");
 
 var A = parseInt (valores.shift());
 var B = parseInt (valores.shift());
