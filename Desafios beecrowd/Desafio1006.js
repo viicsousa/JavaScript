a4531d6cfff11e91a0e42af468cb3d76b6a53528
Victória <victoria.vasconcelos@hotmail.com>
2022-05-25 16:21:00 -0300

    Desafios beecrowd

---
+++ Desafios beecrowd/Desafio1006.js
@@ -0,0 +1,11 @@
+var input = require("fs").readFileSync("teste", "utf8");
+
+var valores = input.split("\n");
+
+var A = parseFloat(valores.shift());
+var B = parseFloat(valores.shift());
+var C = parseFloat(valores.shift());
+
+var MEDIA = (A * 2 + B * 3 + C * 5) / 10.0;
+
+console.log("MEDIA = " + MEDIA.toFixed(1));
