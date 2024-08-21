af52a8e463df8c94ef36241c589fa69a4447f1cd
viicsousa <victoria.vasconcelos@hotmail.com>
2024-08-21 12:05:07 -0300

    feat:

--- DesafiosBeecrowd/Desafio1010.js
+++ DesafiosBeecrowd/Desafio1010.js
@@ -1,8 +1,9 @@
 var input = require('fs').readFileSync('/dev/stdin', 'utf8');
-var [CodPecas, NumDePecas, ValorPeças] = input.split(" ");
 var [line1, line2] = input.split('\n');
+var [CodPecas, NumDePecas, ValorPeças] = input.split(" ");
+
 
 var TotalPecas1 = line1(NumDePecas * ValorPeças);
 var TotalPecas2 = line2(NumDePecas * ValorPeças);
+console.log(NumDePecas);
 
-console.log(line1)
