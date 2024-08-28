81504c42ce8d7cec3a8cdc515fd467fd23266f0f
viicsousa <victoria.vasconcelos@hotmail.com>
2024-08-28 12:10:07 -0300

    feat:

--- DesafiosBeecrowd/Desafio1070.js
+++ DesafiosBeecrowd/Desafio1070.js
@@ -2,7 +2,7 @@ var input = require('fs').readFileSync('/dev/stdin', 'utf8');
 var lines = input.split('\n');
 
 var x = parseInt(lines.shift());
-var cont = 0;
+var cont = 0; //começa a contagem que inicia em 0;
 while (cont < 6) { //while  é uma estrutura de controle de fluxo que permite a repetição de um boloco de código enquanto uma condição específica for verdadeira;
     if (x % 2 == 1) {
         console.log(x);
