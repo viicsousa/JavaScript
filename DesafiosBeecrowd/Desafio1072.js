65d93c2d6c1d695c5eb258becd321318bfb00be2
viicsousa <victoria.vasconcelos@hotmail.com>
2024-08-30 10:45:16 -0300

    feat: add 1072

---
+++ DesafiosBeecrowd/Desafio1072.js
@@ -0,0 +1,8 @@
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+var N = parseInt(lines);
+for (let i = N; N < 1000; i++) {
+  if (10 <i<20){
+    console.log()
+  }
+}
