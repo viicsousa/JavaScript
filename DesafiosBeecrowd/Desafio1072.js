12af1be1eaddacbf25ff437e040b415035fd078b
viicsousa <victoria.vasconcelos@hotmail.com>
2024-09-03 10:43:22 -0300

    feat: ex 1072

--- DesafiosBeecrowd/Desafio1072.js
+++ DesafiosBeecrowd/Desafio1072.js
@@ -1,8 +1,24 @@
 var input = require('fs').readFileSync('/dev/stdin', 'utf8');
-var lines = input.split('\n');
-var N = parseInt(lines);
-for (let i = N; N < 1000; i++) {
-  if (10 <i<20){
-    console.log()
+var valores = input.split('\n').map(item => parseInt(item));
+
+var N = valores[0];
+valores = valores.splice(1);
+// Função principal para resolver o problema
+function contarValoresDentroFora(N, valores) {
+  let dentro = 0; // Contador para valores dentro do intervalo [10, 20]
+  let fora = 0;   // Contador para valores fora do intervalo [10, 20]
+
+  for (let i = 0; i < N; i++) {
+      let X = valores[i];
+      if (X >= 10 && X <= 20) {
+          dentro++;
+      } else {
+          fora++;
+      }
   }
+
+  console.log(dentro + " in");
+  console.log(fora + " out");
 }
+
+contarValoresDentroFora(N, valores);
